d531e995630fd6455911f48700eacef1c3139b68
Pedro Roman <pedro@fusionbreak.com>
2023-04-26 20:21:35 -0400

    Adds missing font and increase font sizes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import "@fontsource/muli"
 import "@fontsource/work-sans"
 import "@fontsource/work-sans/600.css";
 import "@fontsource/work-sans/700.css";
+import "@fontsource/noto-serif-jp"
 import Layout from "./src/components/layout"
 import api from "./src/api"
 window.hummhiveApi = api
--- src/components/Header/NavItem/styled.ts
+++ src/components/Header/NavItem/styled.ts
@@ -2,8 +2,8 @@ import styled from "styled-components"
 
 export const NavLinkContainer = styled.div`
   padding: 0 16px;
-  font-weight: 700;
-  font-size: 16px;
+  font-weight: 600;
+  font-size: 20px;
   font-family: "Work Sans";
   a{
     color: ${props => props.theme.onBackground};
